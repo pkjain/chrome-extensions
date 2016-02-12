2bcc2360e537c86010ebdeee44eb433ce6fa8232
Parveen Kumar <pkjain@gmail.com>
2016-02-12 23:19:17 +0530

    chrome.tabs.getSelected deprecated in favour or chrome.tabs.query

--- hello-world-dom/popup.js
+++ hello-world-dom/popup.js
@@ -3,7 +3,8 @@ function display_h1 (results){
   h1=results;
   document.querySelector("#id1").innerHTML = "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
 }
-chrome.tabs.getSelected(null, function(tab) {
+chrome.tabs.query({active: true}, function(tabs) {
+  var tab = tabs[0];
   tab_title = tab.title;
   chrome.tabs.executeScript(tab.id, {
     code: 'document.querySelector("h1").textContent'
